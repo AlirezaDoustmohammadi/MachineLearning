928ec80fd5581b8bfb0bfd1376dd10d6219cc571
Alireza Doustmohammadi <alirezaald@yahoo.com>
2021-03-05 13:01:40 -0500

    Classification

--- Classification/KNN/ParizonWindow.R
+++ Classification/KNN/ParizonWindow.R
@@ -11,14 +11,13 @@ dat=read.csv("ML_2021.csv",header = TRUE)
 #distance vector
 dist_val=c()
 dist_val_indx=c()
-nearest_dis=c()
 nearest_dis_indx=c()
 #prediction vecotr
 pred_val=c()
 
 #computing diatance
-#knn
-knn=5
+#threshold
+threshold=500
 
 #random sample inde generator
 sample=c(1:dim(dat)[1])
@@ -35,35 +34,32 @@ while (length(checked_sample)<length(sample)) {
   test_sample_vector=unchecked_sample[sample.int(length(unchecked_sample),21)]
   checked_sample=c(checked_sample,test_sample_vector)
   train_sample_vector=sample[! sample %in% test_sample_vector] 
-    
+  
   for (test_sample in test_sample_vector) {
     
     for (train_sample in train_sample_vector) {
-        dis=sqrt(sum((dat[test_sample,1:dim(dat)[2]-1]-dat[train_sample,1:dim(dat)[2]-1])^2))
-        dist_val=c(dist_val,dis)   
-        dist_val_indx=c(dist_val_indx,train_sample)
+      dis=sqrt(sum((dat[test_sample,1:dim(dat)[2]-1]-dat[train_sample,1:dim(dat)[2]-1])^2))
+      dist_val=c(dist_val,dis)   
+      dist_val_indx=c(dist_val_indx,train_sample) 
+      
+      if (dis < threshold) {
+        nearest_dis_indx=c(nearest_dis_indx,train_sample) 
+      }
     }
-    for (indx in 1:knn) {
-      min_dis=min(dist_val)
-      #find min-dist index
-      min_indx=match(min_dis,dist_val)
-      
-      nearest_dis=c(nearest_dis,min_dis)
-      nearest_dis_indx=c(nearest_dis_indx,dist_val_indx[min_indx])
-      
-      #change value
-      dist_val[min_indx]=max(dist_val)
+    if (length(nearest_dis_indx)==0) {
+      nearest_dis_indx=c(nearest_dis_indx,dist_val_indx[match(dist_val,min(dist_val))])
     }
     
     #pridiction
-    knn_lable=c(dat[nearest_dis_indx,dim(dat)[2]])
-    pred_lable=max(knn_lable)
+    parizon_lable=c(dat[nearest_dis_indx,dim(dat)[2]])
+    
+    pred_lable=max(parizon_lable)
     pred_val=c(pred_val,pred_lable)  
+    
     
     #clear data from distance vector
     dist_val=c() 
     dist_val_indx=c()  
-    nearest_dis=c()
     nearest_dis_indx=c()
   }
   true_prediction=0
